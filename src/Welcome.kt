0efe447395cf97bf0bc1b0880c9cd76cf9da3ed3
BornIncompetence <christopher_greer@elcamino.edu>
2018-10-17 15:22:21 -0700

    Created Welcome page as hub for all user interaction

--- src/Welcome.kt
+++ src/Welcome.kt
@@ -1,19 +1,34 @@
 import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.Scene
+import javafx.scene.control.Menu
+import javafx.scene.control.MenuBar
+import javafx.scene.control.MenuItem
 import javafx.scene.layout.GridPane
+import javafx.scene.layout.VBox
 import javafx.scene.text.Text
+import javafx.stage.Modality
+import javafx.stage.Stage
 
-// Main menu:
-//     Modify user account data
-//     Menu bar include account, appointment, setting, and help
+// Main menu
 object Welcome {
 	data class User(val email: String, val password: String) {}
 	lateinit var user: User
+	val stage = Stage()
+
+	// Specifies different types of stages to appear on top of main window
+	private enum class StageType {
+		CREATE_ACCOUNT, CHANGE_USERNAME, CHANGE_PASSWORD, MODIFY_ACCOUNT, SET_CALENDAR_TYPE
+	}
 
 	val scene by lazy {	scene()	}
 
 	private fun scene(): Scene {
+
+		stage.initModality(Modality.APPLICATION_MODAL)
+
+		val vbox = VBox(10.0)
+		vbox.alignment = Pos.TOP_LEFT
 
 		val gridPane = GridPane()
 		gridPane.alignment = Pos.CENTER
@@ -21,14 +36,70 @@ object Welcome {
 		gridPane.vgap = 10.0
 		gridPane.padding = Insets(25.0, 25.0, 25.0, 25.0)
 
-		val email = Text(user.email)
-		email.font = GUIFont.bold
-		gridPane.add(email, 0, 0)
+		val menuBar = MenuBar()
+		val menuAccount = Menu("Account")
+		val menuAppointment = Menu("Appointment")
+		val menuSettings = Menu("Settings")
+		val menuHelp = Menu("Help")
 
-		val password = Text(user.password)
-		password.font = GUIFont.bold
-		gridPane.add(password, 1, 0)
+		val createAccount = MenuItem("Create account")
+		createAccount.setOnAction {
+			_ ->
+			// TODO: Possibly re-purpose Registrar
+			changeStage(StageType.CREATE_ACCOUNT)
+			stage.showAndWait()
+		}
 
-		return Scene(gridPane, 150.0, 50.0)
+		val changeUsername = MenuItem("Change user name")
+		changeUsername.setOnAction {
+			_ ->
+			changeStage(StageType.CHANGE_USERNAME)
+			stage.showAndWait()
+		}
+
+		val changePassword = MenuItem("Change password")
+		changePassword.setOnAction {
+			_ ->
+			changeStage(StageType.CHANGE_PASSWORD)
+			stage.showAndWait()
+		}
+
+		val modifyAccount = MenuItem("Modify account")
+		modifyAccount.setOnAction {
+			_ ->
+			changeStage(StageType.MODIFY_ACCOUNT)
+			stage.showAndWait()
+		}
+
+		menuAccount.items.addAll(createAccount, changeUsername, changePassword, modifyAccount)
+
+		val setCalendarType = MenuItem("Set calendar type")
+		setCalendarType.setOnAction {
+			_ ->
+			// Change how calendar view is organized get either Week, Month, or Day
+			changeStage(StageType.SET_CALENDAR_TYPE)
+		}
+		menuSettings.items.addAll(setCalendarType)
+
+		menuBar.menus.addAll(menuAccount, menuAppointment, menuSettings, menuHelp)
+		vbox.children.add(menuBar)
+
+		val welcomeText = Text("Logged in as " + user.email)
+		welcomeText.font = GUIFont.regularItalic
+		gridPane.add(welcomeText, 0, 1)
+
+		vbox.children.add(gridPane)
+
+		return Scene(vbox, 300.0, 200.0)
+	}
+
+	private fun changeStage(stageType: StageType) {
+		stage.scene = when (stageType) {
+			Welcome.StageType.CREATE_ACCOUNT -> Registrar.scene
+			Welcome.StageType.CHANGE_USERNAME -> TODO()
+			Welcome.StageType.CHANGE_PASSWORD -> TODO()
+			Welcome.StageType.MODIFY_ACCOUNT -> TODO()
+			Welcome.StageType.SET_CALENDAR_TYPE -> TODO()
+		}
 	}
 }
